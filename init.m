322d0d546adc25bcb4cce951ea83996bec7b1814
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-04-08 15:25:04 +0100

    Add init file

---
+++ init.m
@@ -0,0 +1,14 @@
+% FILE:         init.m
+% DESCRIPTION:  Initialise environments
+% AUTHOR:       James Bennion-Pedley
+% DATE CREATED: 08/04/2022
+
+%------------------------------------------------------------------------------%
+
+close all; clc; clear;
+
+%-------------------------------- Entry Point ---------------------------------%
+
+addpath("src", "matlab_tools/tools");
+
+%------------------------------ Helper Functions ------------------------------%
