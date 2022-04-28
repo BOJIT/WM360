b50a99c2120ca05a016d2b47772c3bab38bedc13
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-04-28 09:10:02 +0100

    Unstage code-snippets

---
+++ src/fir.m
@@ -0,0 +1,14 @@
+% FILE:         fir.m
+% DESCRIPTION:  FIR Filter Testing
+% AUTHOR:       James Bennion-Pedley
+% DATE CREATED: 28/04/2022
+
+%------------------------------------------------------------------------------%
+
+close all; clc; clear;
+
+%-------------------------------- Entry Point ---------------------------------%
+
+dsp = DSP();
+
+%------------------------------ Helper Functions ------------------------------%
