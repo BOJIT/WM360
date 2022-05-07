a44ddf79dc68c8c8fd2d5d23625152d2ff2fe6b5
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-05-07 11:38:18 +0100

    Add Simulation Class with props

--- src/Simulation.m
+++ src/Simulation.m
@@ -9,7 +9,9 @@ classdef Simulation < handle
 
     %---------------------------- Public Properties ---------------------------%
     properties
-
+        DecimationFactor = 3;
+        SampleRate = 48000;
+        Scheme = 'linear';
     end
 
     properties (Dependent)
@@ -18,7 +20,18 @@ classdef Simulation < handle
 
     %---------------------------- Private Properties --------------------------%
     properties (Access = private)
+        EncoderPath = 'Simulink/PCM_Encoder.slx';
+        DecoderPath = 'Simulink/PCM_Decoder.slx';
 
+        EncoderSim;
+        DecoderSim;
+
+        % List of object properties that get written to Simulink workspace
+        Params = [ ...
+            "DecimationFactor", ...
+            "SampleRate", ...
+            "Scheme", ...
+        ];
     end
 
     properties (Access = private, Dependent)
@@ -27,8 +40,23 @@ classdef Simulation < handle
 
     %------------------------------- Constructor ------------------------------%
     methods
-        function obj = Simulation()
+        function obj = Simulation(decimation_factor, sample_rate, scheme)
+            obj.EncoderSim = Simulink.SimulationInput(obj.EncoderPath);
+            obj.DecoderSim = Simulink.SimulationInput(obj.DecoderPath);
 
+            if nargin >= 1
+                obj.DecimationFactor = decimation_factor;
+            end
+
+            if nargin >= 2
+                obj.SampleRate = sample_rate;
+            end
+
+            if nargin >= 3
+                obj.Scheme = scheme;
+            end
+
+            obj.setConfig();
         end
     end
 
@@ -39,12 +67,30 @@ classdef Simulation < handle
 
     %------------------------------ Private Methods ---------------------------%
     methods
-
+        function setConfig(obj)
+            for p = obj.Params
+                obj.EncoderSim.setVariable(p, obj.(p), 'Workspace', obj.EncoderSim.ModelName);
+                obj.DecoderSim.setVariable(p, obj.(p), 'Workspace', obj.DecoderSim.ModelName);
+            end
+        end
     end
 
     %------------------------------ Get/Set Methods ---------------------------%
     methods
+        function set.DecimationFactor(obj, val)
+            obj.DecimationFactor = val;
+            obj.setConfig();
+        end
 
+        function set.SampleRate(obj, val)
+            obj.SampleRate = val;
+            obj.setConfig();
+        end
+
+        function set.Scheme(obj, val)
+            obj.Scheme = val;
+            obj.setConfig();
+        end
     end
 
 end
